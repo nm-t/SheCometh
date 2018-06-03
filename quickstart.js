ca13d3faf5ca35293ccd5b28e10a7aa407e9cb84
nmt <nathaliamtan@gmail.com>
2018-06-03 22:35:20 +1000

    Calculates new date

--- quickstart.js
+++ quickstart.js
@@ -3,7 +3,7 @@ const readline = require('readline');
 const {google} = require('googleapis');
 
 // If modifying these scopes, delete credentials.json.
-const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
+const SCOPES = ['https://www.googleapis.com/auth/calendar'];
 const TOKEN_PATH = 'credentials.json';
 
 // Load client secrets from a local file.
@@ -25,13 +25,13 @@ function authorize(credentials, callback) {
   const {client_secret, client_id, redirect_uris} = credentials.installed;
   let token = {};
   const oAuth2Client = new google.auth.OAuth2(
-      client_id, client_secret, redirect_uris[0]);
+	  client_id, client_secret, redirect_uris[0]);
 
   // Check if we have previously stored a token.
   try {
-    token = fs.readFileSync(TOKEN_PATH);
+	token = fs.readFileSync(TOKEN_PATH);
   } catch (err) {
-    return getAccessToken(oAuth2Client, callback);
+	return getAccessToken(oAuth2Client, callback);
   }
   oAuth2Client.setCredentials(JSON.parse(token));
   callback(oAuth2Client);
@@ -45,28 +45,28 @@ function authorize(credentials, callback) {
  */
 function getAccessToken(oAuth2Client, callback) {
   const authUrl = oAuth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: SCOPES,
+	access_type: 'offline',
+	scope: SCOPES,
   });
   console.log('Authorize this app by visiting this url:', authUrl);
   const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
+	input: process.stdin,
+	output: process.stdout,
   });
   rl.question('Enter the code from that page here: ', (code) => {
-    rl.close();
-    oAuth2Client.getToken(code, (err, token) => {
-      if (err) return callback(err);
-      oAuth2Client.setCredentials(token);
-      // Store the token to disk for later program executions
-      try {
-        fs.writeFileSync(TOKEN_PATH, JSON.stringify(token));
-        console.log('Token stored to', TOKEN_PATH);
-      } catch (err) {
-        console.error(err);
-      }
-      callback(oAuth2Client);
-    });
+	rl.close();
+	oAuth2Client.getToken(code, (err, token) => {
+	  if (err) return callback(err);
+	  oAuth2Client.setCredentials(token);
+	  // Store the token to disk for later program executions
+	  try {
+		fs.writeFileSync(TOKEN_PATH, JSON.stringify(token));
+		console.log('Token stored to', TOKEN_PATH);
+	  } catch (err) {
+		console.error(err);
+	  }
+	  callback(oAuth2Client);
+	});
   });
 }
 
@@ -75,24 +75,52 @@ function getAccessToken(oAuth2Client, callback) {
  * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
  */
 function listEvents(auth) {
-  const calendar = google.calendar({version: 'v3', auth});
-  calendar.events.list({
-    calendarId: 'primary',
-    timeMin: (new Date()).toISOString(),
-    maxResults: 10,
-    singleEvents: true,
-    orderBy: 'startTime',
-  }, (err, {data}) => {
-    if (err) return console.log('The API returned an error: ' + err);
-    const events = data.items;
-    if (events.length) {
-      console.log('Upcoming 10 events:');
-      events.map((event, i) => {
-        const start = event.start.dateTime || event.start.date;
-        console.log(`${start} - ${event.summary}`);
-      });
-    } else {
-      console.log('No upcoming events found.');
-    }
-  });
+	const calendar = google.calendar({version: 'v3', auth});
+	calendar.events.list({
+		calendarId: 'primary',
+		timeMin: new Date(new Date()-2592000000).toISOString(), // 1 month in the past
+		singleEvents: true,
+		orderBy: 'startTime',
+	}, (err, {data}) => {
+		if (err) return console.log('The API returned an error: ' + err);
+		const events = data.items;
+		
+		if (events.length) {
+			console.log('The crimson tide last struck:');
+			events.map((event, i) => {
+				if (event.summary === 'She Cometh'){
+					// Get most recent She Cometh
+					const start = event.start.dateTime || event.start.date;
+					console.log(`${start} - ${event.summary}`);
+
+					// Check it hasn't been modified for 10 days
+					let today = new Date();
+					let daysUntouched = 5 * 24 * 60 * 60 * 1000;
+					if (event.updated < (new Date(today - daysUntouched))){
+						console.log('she old');
+					}
+
+					// Create the new event for next month
+					const end = event.end.dateTime || event.end.date;
+					PlanTheComing(end);
+				}
+			});
+		}
+		
+		else {
+		  console.log('She has not cometh.');
+		}
+	});
+}
+
+/**
+ * Creates a new She Cometh event for 28 days in the future from the last one
+ * @param {date} lastDay 
+ */
+function PlanTheComing(lastDay) {
+	let offset = 28 * 24 * 60 * 60 * 1000;
+	var lastDay = new Date(lastDay);
+	var newDate = new Date(lastDay.getTime() + offset);
+
+	// Create new event
 }
